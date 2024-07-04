fa8a716f2efdf9da4cf7e6099f46ddb25bf88967
MrJacob12 <jakub.buciuto@gmail.com>
2024-07-04 15:36:30 +0200

    feat: Update InventoryUIElement to handle selling items

--- Assets/_Scripts/UI/InventoryUIElement.cs
+++ Assets/_Scripts/UI/InventoryUIElement.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 public class InventoryUIElement : MonoBehaviour
 {
     [SerializeField]
+    private GameObject itemPanel;
+    [SerializeField]
     private string itemName;
     [Header("UI Elements")]
     public Text itemNameText;
@@ -13,6 +15,14 @@ public class InventoryUIElement : MonoBehaviour
 
     private void Start()
     {
+        if (InventorySystem.Instance.items.ContainsKey(itemName) && InventorySystem.Instance.items[itemName].amount > 0)
+        {
+            itemPanel.SetActive(true);
+        }
+        else
+        {
+            itemPanel.SetActive(false);
+        }
         itemNameText.text = itemName;
         UpdateAmount();
     }
@@ -30,6 +40,17 @@ public class InventoryUIElement : MonoBehaviour
         else
         {
             itemAmountText.text = "0";
+        }
+    }
+
+    public void SellItem()
+    {
+        if (InventorySystem.Instance.items.ContainsKey(itemName))
+        {
+            InventoryItem item = InventorySystem.Instance.items[itemName];
+            MoneySystem.Instance.AddMoney(item.sellPrice * item.amount);
+            InventorySystem.Instance.RemoveItem(itemName, item.amount);
+            SaveSystem.Instance.Save();
         }
     }
 }
