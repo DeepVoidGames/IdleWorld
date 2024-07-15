69995e0aec11a46109bbd48f1c23aaf750a965b4
MrJacob12 <jakub.buciuto@gmail.com>
2024-07-15 18:29:13 +0200

    refactor: Update cave upgrades UI, rock base health values, indicator UI elements, crafting recipes, pickaxe items, pickaxe item icons, inventory sorting, rocks data loading and spawning logic, and BiomeSystem loading and setting current biomes

--- Assets/_Scripts/System/UISystem.cs
+++ Assets/_Scripts/System/UISystem.cs
@@ -79,417 +79,54 @@ public class UISystem : MonoBehaviour {
 
     public string NumberFormat(double number)
     {
-        if (number >= 1e308)
-        {
-            return (number / 1e308).ToString("F2") + "G";
-        }
-        if (number >= 1e303)
-        {
-            return (number / 1e303).ToString("F2") + "V";
-        }
-        if (number >= 1e300)
-        {
-            return (number / 1e300).ToString("F2") + "W";
-        }
-        if (number >= 1e297)
-        {
-            return (number / 1e297).ToString("F2") + "X";
-        }
-        if (number >= 1e294)
-        {
-            return (number / 1e294).ToString("F2") + "Y";
-        }
-        if (number >= 1e291)
-        {
-            return (number / 1e291).ToString("F2") + "Z";
-        }
-        if (number >= 1e288)
-        {
-            return (number / 1e288).ToString("F2") + "U";
-        }
-        if (number >= 1e285)
-        {
-            return (number / 1e285).ToString("F2") + "S";
-        }
-        if (number >= 1e282)
-        {
-            return (number / 1e282).ToString("F2") + "R";
-        }
-        if (number >= 1e279)
-        {
-            return (number / 1e279).ToString("F2") + "Q";
-        }
-        if (number >= 1e276)
-        {
-            return (number / 1e276).ToString("F2") + "P";
-        }
-        if (number >= 1e273)
-        {
-            return (number / 1e273).ToString("F2") + "O";
-        }
-        if (number >= 1e270)
-        {
-            return (number / 1e270).ToString("F2") + "N";
-        }
-        if (number >= 1e267)
-        {
-            return (number / 1e267).ToString("F2") + "M";
-        }
-        if (number >= 1e264)
-        {
-            return (number / 1e264).ToString("F2") + "L";
-        }
-        if (number >= 1e261)
-        {
-            return (number / 1e261).ToString("F2") + "K";
-        }
-        if (number >= 1e258)
-        {
-            return (number / 1e258).ToString("F2") + "J";
-        }
-        if (number >= 1e255)
-        {
-            return (number / 1e255).ToString("F2") + "I";
-        }
-        if (number >= 1e252)
-        {
-            return (number / 1e252).ToString("F2") + "H";
-        }
-        if (number >= 1e249)
-        {
-            return (number / 1e249).ToString("F2") + "G";
-        }
-        if (number >= 1e246)
-        {
-            return (number / 1e246).ToString("F2") + "F";
-        }
-        if (number >= 1e243)
-        {
-            return (number / 1e243).ToString("F2") + "E";
-        }
-        if (number >= 1e240)
-        {
-            return (number / 1e240).ToString("F2") + "D";
-        }
-        if (number >= 1e237)
-        {
-            return (number / 1e237).ToString("F2") + "C";
-        }
-        if (number >= 1e234)
-        {
-            return (number / 1e234).ToString("F2") + "B";
-        }
-        if (number >= 1e231)
-        {
-            return (number / 1e231).ToString("F2") + "A";
-        }
-        if (number >= 1e228)
-        {
-            return (number / 1e228).ToString("F2") + "Z";
-        }
-        if (number >= 1e225)
-        {
-            return (number / 1e225).ToString("F2") + "Y";
-        }
-        if (number >= 1e222)
-        {
-            return (number / 1e222).ToString("F2") + "X";
-        }
-        if (number >= 1e219)
-        {
-            return (number / 1e219).ToString("F2") + "W";
-        }
-        if (number >= 1e216)
-        {
-            return (number / 1e216).ToString("F2") + "V";
-        }
-        if (number >= 1e213)
-        {
-            return (number / 1e213).ToString("F2") + "U";
-        }
-        if (number >= 1e210)
-        {
-            return (number / 1e210).ToString("F2") + "T";
-        }
-        if (number >= 1e207)
-        {
-            return (number / 1e207).ToString("F2") + "S";
-        }
-        if (number >= 1e204)
-        {
-            return (number / 1e204).ToString("F2") + "R";
-        }
-        if (number >= 1e201)
-        {
-            return (number / 1e201).ToString("F2") + "Q";
-        }
-        if (number >= 1e198)
-        {
-            return (number / 1e198).ToString("F2") + "P";
-        }
-        if (number >= 1e195)
-        {
-            return (number / 1e195).ToString("F2") + "O";
-        }
-        if (number >= 1e192)
-        {
-            return (number / 1e192).ToString("F2") + "N";
-        }
-        if (number >= 1e189)
-        {
-            return (number / 1e189).ToString("F2") + "M";
-        }
-        if (number >= 1e186)
-        {
-            return (number / 1e186).ToString("F2") + "L";
-        }
-        if (number >= 1e183)
-        {
-            return (number / 1e183).ToString("F2") + "K";
-        }
-        if (number >= 1e180)
-        {
-            return (number / 1e180).ToString("F2") + "J";
-        }
-        if (number >= 1e177)
-        {
-            return (number / 1e177).ToString("F2") + "I";
-        }
-        if (number >= 1e174)
-        {
-            return (number / 1e174).ToString("F2") + "H";
-        }
-        if (number >= 1e171)
-        {
-            return (number / 1e171).ToString("F2") + "G";
-        }
-        if (number >= 1e168)
-        {
-            return (number / 1e168).ToString("F2") + "F";
-        }
-        if (number >= 1e165)
-        {
-            return (number / 1e165).ToString("F2") + "E";
-        }
-        if (number >= 1e162)
-        {
-            return (number / 1e162).ToString("F2") + "D";
-        }
-        if (number >= 1e159)
-        {
-            return (number / 1e159).ToString("F2") + "C";
-        }
-        if (number >= 1e156)
-        {
-            return (number / 1e156).ToString("F2") + "B";
-        }
-        if (number >= 1e153)
-        {
-            return (number / 1e153).ToString("F2") + "A";
-        }
-        if (number >= 1e150)
-        {
-            return (number / 1e150).ToString("F2") + "Z";
-        }
-        if (number >= 1e147)
-        {
-            return (number / 1e147).ToString("F2") + "Y";
-        }
-        if (number >= 1e144)
-        {
-            return (number / 1e144).ToString("F2") + "X";
-        }
-        if (number >= 1e141)
-        {
-            return (number / 1e141).ToString("F2") + "W";
-        }
-        if (number >= 1e138)
-        {
-            return (number / 1e138).ToString("F2") + "V";
-        }
-        if (number >= 1e135)
-        {
-            return (number / 1e135).ToString("F2") + "U";
-        }
-        if (number >= 1e132)
-        {
-            return (number / 1e132).ToString("F2") + "T";
-        }
-        if (number >= 1e129)
-        {
-            return (number / 1e129).ToString("F2") + "S";
-        }
-        if (number >= 1e126)
-        {
-            return (number / 1e126).ToString("F2") + "R";
-        }
-        if (number >= 1e123)
-        {
-            return (number / 1e123).ToString("F2") + "Q";
-        }
-        if (number >= 1e120)
-        {
-            return (number / 1e120).ToString("F2") + "P";
-        }
-        if (number >= 1e117)
-        {
-            return (number / 1e117).ToString("F2") + "O";
-        }
-        if (number >= 1e114)
-        {
-            return (number / 1e114).ToString("F2") + "N";
-        }
-        if (number >= 1e111)
-        {
-            return (number / 1e111).ToString("F2") + "M";
-        }
-        if (number >= 1e108)
-        {
-            return (number / 1e108).ToString("F2") + "L";
-        }
-        if (number >= 1e105)
-        {
-            return (number / 1e105).ToString("F2") + "K";
-        }
-        if (number >= 1e102)
-        {
-            return (number / 1e102).ToString("F2") + "J";
-        }
-        if (number >= 1e99)
-        {
-            return (number / 1e99).ToString("F2") + "I";
-        }
-        if (number >= 1e96)
-        {
-            return (number / 1e96).ToString("F2") + "H";
-        }
-        if (number >= 1e93)
-        {
-            return (number / 1e93).ToString("F2") + "G";
-        }
-        if (number >= 1e90)
-        {
-            return (number / 1e90).ToString("F2") + "F";
-        }
-        if (number >= 1e87)
-        {
-            return (number / 1e87).ToString("F2") + "E";
-        }
-        if (number >= 1e84)
-        {
-            return (number / 1e84).ToString("F2") + "D";
-        }
-        if (number >= 1e81)
-        {
-            return (number / 1e81).ToString("F2") + "C";
-        }
-        if (number >= 1e78)
-        {
-            return (number / 1e78).ToString("F2") + "B";
-        }
-        if (number >= 1e75)
-        {
-            return (number / 1e75).ToString("F2") + "A";
-        }
-        if (number >= 1e72)
-        {
-            return (number / 1e72).ToString("F2") + "Z";
-        }
-        if (number >= 1e69)
-        {
-            return (number / 1e69).ToString("F2") + "Y";
-        }
-        if (number >= 1e66)
-        {
-            return (number / 1e66).ToString("F2") + "X";
-        }
-        if (number >= 1e63)
-        {
-            return (number / 1e63).ToString("F2") + "W";
-        }
-        if (number >= 1e60)
-        {
-            return (number / 1e60).ToString("F2") + "V";
-        }
-        if (number >= 1e57)
-        {
-            return (number / 1e57).ToString("F2") + "U";
-        }
-        if (number >= 1e54)
-        {
-            return (number / 1e54).ToString("F2") + "T";
-        }
-        if (number >= 1e51)
-        {
-            return (number / 1e51).ToString("F2") + "S";
-        }
-        if (number >= 1e48)
-        {
-            return (number / 1e48).ToString("F2") + "R";
-        }
-        if (number >= 1e45)
-        {
-            return (number / 1e45).ToString("F2") + "Q";
-        }
-        if (number >= 1e42)
-        {
-            return (number / 1e42).ToString("F2") + "P";
-        }
-        if (number >= 1e39)
-        {
-            return (number / 1e39).ToString("F2") + "O";
-        }
-        if (number >= 1e36)
-        {
-            return (number / 1e36).ToString("F2") + "N";
-        }
-        if (number >= 1e33)
-        {
-            return (number / 1e33).ToString("F2") + "M";
-        }
-        if (number >= 1e30)
-        {
-            return (number / 1e30).ToString("F2") + "L";
-        }
-        if (number >= 1e27)
-        {
-            return (number / 1e27).ToString("F2") + "K";
-        }
-        if (number >= 1e24)
-        {
-            return (number / 1e24).ToString("F2") + "Y";
-        }
-        if (number >= 1e21)
-        {
-            return (number / 1e21).ToString("F2") + "Z";
-        }
-        if (number >= 1e18)
-        {
-            return (number / 1e18).ToString("F2") + "E";
-        }
-        if (number >= 1e15)
-        {
-            return (number / 1e15).ToString("F2") + "P";
-        }
-        if (number >= 1e12)
-        {
-            return (number / 1e12).ToString("F2") + "T";
-        }
-        if (number >= 1e9)
-        {
-            return (number / 1e9).ToString("F2") + "B";
-        }
-        if (number >= 1e6)
-        {
-            return (number / 1e6).ToString("F2") + "M";
-        }
-        if (number >= 1e3)
-        {
-            return (number / 1e3).ToString("F2") + "K";
-        }
-        return number.ToString("F2");
+        if (number < 1e3)
+        {
+            return number.ToString("F2");
+        }
+
+        string[] commonSuffixes = { "K", "M", "B" };
+        var suffixes = GenerateSuffixes();
+
+        double divisor = 1;
+        int commonSuffixCount = commonSuffixes.Length;
+        int suffixIndex = 0;
+
+        for (int exp = 3; exp <= 300; exp += 3)
+        {
+            divisor = Math.Pow(10, exp);
+
+            if (number < divisor * 1000)
+            {
+                if (suffixIndex < commonSuffixCount)
+                {
+                    return (number / divisor).ToString("F2") + commonSuffixes[suffixIndex];
+                }
+                else
+                {
+                    return (number / divisor).ToString("F2") + suffixes[suffixIndex - commonSuffixCount];
+                }
+            }
+            suffixIndex++;
+        }
+        
+        // In case the number is larger than all defined ranges
+        return (number / divisor).ToString("F2") + suffixes[suffixIndex - commonSuffixCount];
     }
 
+    private List<string> GenerateSuffixes()
+    {
+        var suffixes = new List<string>();
+        // Double letter suffixes
+        for (char first = 'A'; first <= 'Z'; first++)
+        {
+            for (char second = 'A'; second <= 'Z'; second++)
+            {
+                suffixes.Add(first.ToString() + second.ToString());
+            }
+        }
+        return suffixes;
+    }
+    
     public string NumberFormatInt(int number)
     {
         if (number >= 1000000000)
